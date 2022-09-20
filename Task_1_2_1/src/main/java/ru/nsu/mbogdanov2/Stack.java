9d4291be3ee0c780f0ca21894278d459f7986639
Миша <91488621+Allody22@users.noreply.github.com>
2022-09-20 22:48:24 +0700

    Update Stack.java
    new fixes

--- Task_1_2_1/src/main/java/ru/nsu/mbogdanov2/Stack.java
+++ Task_1_2_1/src/main/java/ru/nsu/mbogdanov2/Stack.java
@@ -8,81 +8,86 @@ import java.util.Arrays;
  *
  *@author Михаил Allody22 Богданов
  */
-public class Stack<T> { 
-  int top;
-  T[] stack;
-  /**
-   * Stack itself.
-   * with array stack to contain elements
-   * and int variable top to get last element
-   */
-  public Stack() {
-    stack = (T[]) new Object[1];
-    top = -1;
-  }
-  /**
-   * push function to put new element into stack
-   *
-   * @param element - any variable of type T
-   */
+public class Stack<T> {
 
-  public void push(T element) {
-    if ((top+1) == stack.length) {
-      stack = Arrays.copyOf(stack, stack.length + 1);
+    int top;
+    T[] stack;
+
+    /**
+     * Stack itself. with array stack to contain elements and int variable top to get last element
+     */
+    public Stack() {
+      stack = (T[]) new Object[1];
+      top = -1;
     }
-    stack[++top] = element;
-  }
-  /**
-   * pop function to delete last stack element
-   * with error out of bounds handling that fails the test
-   */
 
-  public void pop() {
-    try {
-      stack[top--] = null;
+    /**
+     * push function to put new element into stack
+     *
+     * @param element - any variable of type T
+     */
+
+    public void push(T element) {
+      if ((top + 1) == stack.length) {
+        stack = Arrays.copyOf(stack, stack.length + 1);
+      }
+      stack[++top] = element;
     }
-    catch (ArrayIndexOutOfBoundsException exception) {
-      throw new AssertionError("Stack is already empty,don't use pop when top = 0\n"
-          + "Test failed, because of last pop");
+
+    /**
+     * pop function to delete last stack element with error out of bounds handling that fails the
+     * test
+     */
+
+    public void pop() {
+      try {
+        stack[top--] = null;
+      } catch (ArrayIndexOutOfBoundsException exception) {
+        throw new AssertionError("Stack is already empty,don't use pop when top = 0\n"
+            + "Test failed, because of last pop");
+      }
     }
-  }
-  /**
-   * count function to get the number of elements in stack
-   */
 
-  public int count() {
-    return top+1;
-  }
-  /**
-   * pushStack function to put several new elements into stack
-   *
-   * @param NewStack - new array to combine its elements
-   */
+    /**
+     * count function to get the number of elements in stack
+     */
 
-  public void pushStack(Stack<T> NewStack) {
-    for (int i = 0; i < NewStack.stack.length; i++) {
-      push(NewStack.stack[i]);
+    public int count() {
+      return top + 1;
     }
-  }
-  /**
-   * popStack function to delete several last stack elements
-   * with error out of bounds handling that fails the test
-   *
-   * @param N - number of elements to delete
-   */
 
-  public void popStack(int N) {
-    for (int i = 0; i < N; i++) {
-      pop();
+    /**
+     * pushStack function to put several new elements into stack
+     *
+     * @param NewStack - new array to combine its elements
+     */
+
+    public void pushStack(Stack<T> NewStack) {
+      for (int i = 0; i < NewStack.stack.length; i++) {
+        push(NewStack.stack[i]);
+      }
     }
-  }
-  /**
-   * Override of toString method in order for the checks
-   * to work correctly with generics arrays, to compare them
-   */
 
-  @Override
-  public String toString() {
-    return Arrays.toString(Arrays.copyOf(stack, top+1));
-  }
+    /**
+     * popStack function to delete several last stack elements with error out of bounds handling that
+     * fails the test
+     *
+     * @param N - number of elements to delete
+     */
+
+    public void popStack(int N) {
+      for (int i = 0; i < N; i++) {
+        pop();
+      }
+    }
+
+    /**
+     * Override of toString method in order for the checks to work correctly with generics arrays, to
+     * compare them
+     */
+
+    @Override
+    public String toString() {
+      return Arrays.toString(Arrays.copyOf(stack, top + 1));
+    }
 }
