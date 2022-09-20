da6061719e98c57e3f93680a004b63028991a063
Миша <m.bogdanov2@g.nsu.ru>
2022-09-20 21:52:28 +0700

    Script itself

---
+++ Task_1_1_1/script.bat
@@ -0,0 +1,6 @@
+javadoc -d .\make\ -sourcepath .\src\main\java -subpackages ru.nsu.mbogdanov2
+
+javac -d .\make\bin\ -sourcepath .\src\main\java .\src\main\java\ru\nsu\mbogdanov2\HeapSort.java
+
+mkdir .\make\jar
+jar cf .\make\jar\HeapSort.jar -C .\make\bin .
