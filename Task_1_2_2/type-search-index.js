42ede8247cd6125cd66f76188f3f7600489dd34a
Allody22 <Allody22@users.noreply.github.com>
2022-10-09 13:10:03 +0000

    Deploying to gh-pages from @ Allody22/OOP_JAVA@47488244bba8b1b1a437354c39079036ebe80abc 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.mbogdanov2","l":"BreadthFirstSearchIterator"},{"p":"ru.nsu.mbogdanov2","l":"DeepFirstSearchIterator"},{"p":"ru.nsu.mbogdanov2","l":"Node"},{"p":"ru.nsu.mbogdanov2","l":"Tree"}]
