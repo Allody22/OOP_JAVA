9ad071b9f588da04d60d369914bacb55e18ec6f5
Миша <m.bogdanov2@g.nsu.ru>
2022-10-10 20:20:01 +0700

    Last code stile fixes

--- Task_1_2_2/src/main/java/ru/nsu/mbogdanov2/Node.java
+++ Task_1_2_2/src/main/java/ru/nsu/mbogdanov2/Node.java
@@ -114,10 +114,15 @@ public class Node<T> {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Node)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Node)) {
+            return false;
+        }
         Node<?> node = (Node<?>) o;
-        return Objects.equals(value, node.value) && Objects.equals(listOfChildren, node.listOfChildren) && Objects.equals(parent, node.parent);
+        return Objects.equals(value, node.value) && Objects.
+                equals(listOfChildren, node.listOfChildren) && Objects.equals(parent, node.parent);
     }
 
     /**
