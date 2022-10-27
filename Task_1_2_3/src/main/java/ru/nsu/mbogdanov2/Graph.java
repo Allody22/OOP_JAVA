acc2d0a416b039be27c14bb41cd5b0ff61b98efd
Миша <m.bogdanov2@g.nsu.ru>
2022-10-27 21:43:55 +0700

    all methods and start of dijkstra

--- Task_1_2_3/src/main/java/ru/nsu/mbogdanov2/Graph.java
+++ Task_1_2_3/src/main/java/ru/nsu/mbogdanov2/Graph.java
@@ -5,11 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Graph<T extends Comparable<T>>{
-
+/**
+ * We implement directed weighted graph.
+ * Three different constructors for matrix of incidents and adjacency
+ * and for list of adjacency
+ *
+ * @param <T>
+ */
+public class Graph<T extends Comparable<T>> {
 
     private final Map<T, Vertex<T>> vertexes;
     private final Map<T, List<Edge<T>>> edges;
+    private final int INF = Integer.MAX_VALUE;
 
     public Graph() {
         vertexes = new HashMap<>();
@@ -17,7 +24,12 @@ public class Graph<T extends Comparable<T>>{
     }
 
     /**
-     * Матрица смежности.
+     * Matrix of adjacency.
+     * Matrix is in a row-major implemented
+     *  example of row-major matrix
+     *   |a b c |
+     *   |d e f |   =>  [a,b,c,d,e,f,j,k,l]
+     *   |j k d |
      *
      * @param vertexArray - вершины
      * @param matrix - матрица
@@ -27,24 +39,118 @@ public class Graph<T extends Comparable<T>>{
         edges = new HashMap<>();
 
         int len = vertexArray.length;
-        /*addAllVertexes(vertexArray);
+        for (T t : vertexArray) {
+            addVertex(t);
+        }
 
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
-                addEdge(vertexArray[i], vertexArray[j], matrix[i][j]);
+                addEdge(vertexArray[i], vertexArray[j], matrix[i*len+j]);
             }
-        }*/
+        }
     }
 
-
-    public Graph(T[] vertexArray, List<T> listOfAdjacency) {
+    /**
+     * Matrix of incident.
+     * If there are only one positive or ine negative weight
+     * We think that it's a loop
+     * Matrix is in a row-major implemented
+     * example of row-major matrix
+     *   |a b c |
+     *   |d e f |   =>  [a,b,c,d,e,f,j,k,l]
+     *   |j k d |
+     *
+     * @param vertexArray array of vertexes name in matrix
+     * @param incidentMatrix matrix of incident itself
+     * @param edgeNumber number of edges
+     */
+    public Graph(T[] vertexArray, int[] incidentMatrix, int edgeNumber) {
         vertexes = new HashMap<>();
         edges = new HashMap<>();
 
         int len = vertexArray.length;
+        for (T t : vertexArray) {
+            addVertex(t);
+        }
 
+        for (int i = 0; i < edgeNumber; i++) {
+            int weight = 0;
+            T k1 = null;
+            T k2 = null;
+            for (int j = 0; j < len; j++) {
+                if (incidentMatrix[j*edgeNumber+i] > 0) {
+                    k1 = vertexArray[j];
+                    weight = incidentMatrix[j*edgeNumber+i];
+                }
+                if (incidentMatrix[j*edgeNumber+i] < 0) {
+                    k2 = vertexArray[j];
+                    weight = Math.abs(incidentMatrix[j*edgeNumber+i]);
+                }
+            }
+            if (k1 != null && k2 != null) {
+                addEdge(k1, k2, weight);
+            }
+            if (k1 == null && k2 != null) {
+                addEdge(k2, k2, weight);
+            }
+            if (k1 != null && k2 == null) {
+                addEdge(k1, k1, weight);
+            }
+        }
     }
 
+    /**
+     * Graph representation from list of adjacency.
+     *
+     * @param vertexArray array of existing vertexes
+     * @param vertexList list of adjacency
+     * @param weights weights of the edges
+     */
+    public Graph(T[] vertexArray, List<T>[] vertexList, List<Integer>[] weights) {
+        vertexes = new HashMap<>();
+        edges = new HashMap<>();
+
+        int len = vertexArray.length;
+        for (T t : vertexArray) {
+            addVertex(t);
+        }
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < vertexList[i].size(); j++) {
+                addEdge(vertexArray[i], vertexList[i].get(j), weights[i].get(j));
+            }
+        }
+    }
+
+    public List<Vertex<T>> dijkstra(T vertexStart){
+        if (!vertexes.containsKey(vertexStart)) {
+            throw new IllegalArgumentException("No vertex with this name");
+
+            Vertex<T> start = vertexes.get(vertexStart);
+
+            List<Vertex<T>> distanceArray = new ArrayList<>();
+
+            vertexes.forEach((k, v) -> {
+                distanceArray.add(v);
+                v.setVisited(false);
+                v.setDistance(INF);
+            });
+
+        }
+        /*
+        D[*] = +infinity,  mark[*] = W
+        D[s] = 0, mark[s] = G
+        while (gray vertex exists):
+            u = vertex such that: 1) mark[u] = G
+                                    2) D[u] = min
+        mark[u] = B
+        for uv in E:
+            if D[v] > D[u] + w(uv):
+            D[v] = D[u] + w(uv)
+            F[v] = u
+            mark[v] = G
+         */
+    }
     /**
      * Method to add new vertex to the graph.
      * If vertex with this name is already exist
@@ -57,6 +163,7 @@ public class Graph<T extends Comparable<T>>{
             return;
         }
         Vertex<T> v = new Vertex<>(name);
+        edges.put(v.getName(), new ArrayList<>());
         vertexes.put(name, v);
     }
 
@@ -65,7 +172,7 @@ public class Graph<T extends Comparable<T>>{
      *
      * @param name1 name of the first vertex
      * @param name2 name of the second vertex
-     * @param weight positive weight of the vertex
+     * @param weight positive weights of the vertex
      */
     public void addEdge(T name1, T name2, Integer weight) {
         if (weight <= 0 || name1 == null || name2 == null){
@@ -77,9 +184,6 @@ public class Graph<T extends Comparable<T>>{
 
         Edge<T> e = new Edge<>(v1, v2, weight);
 
-        edges.put(v1.getName(), new ArrayList<>());
-        edges.put(v2.getName(), new ArrayList<>());
-
         edges.get(v1.getName()).add(e);
     }
 
@@ -88,41 +192,37 @@ public class Graph<T extends Comparable<T>>{
      * Function throw null pointer exception is key is null
      * We delete all edges from and to this vertex
      *
-     * @param key key of the vertex
+     * @param name key of the vertex
      * @return deleted vertex
      */
-    public Vertex<T> deleteVertex(T key) {
-        if (vertexes.get(key) == null){
+    public Vertex<T> deleteVertex(T name) {
+        if (vertexes.get(name) == null){
                 throw new NullPointerException("This key is null, don't try to use it");
         }
-        edges.remove(key);
-        Vertex<T> deletedVertex = vertexes.get(key);
-        vertexes.remove(key);
+        if(!vertexes.containsKey(name)) {
+            throw new IllegalArgumentException("No vertex with this name");
+        }
+        edges.remove(name);
+        var deletedVertex = vertexes.get(name);
+        vertexes.remove(name);
         return deletedVertex;
     }
+
+    /**
+     * Deleting edge from the graph.
+     * We remove the edge, but don't do anything with vertexes
+     *
+     * @param name1 name of the first vertex
+     * @param name2 name of incoming vertex
+     * @return return deleted edge or throw exception
+     */
+    public Edge<T> deleteEdge(T name1, T name2) {
+        List<Edge<T>> listOfEdges = edges.get(name1);
+        for (int i = 0; i < listOfEdges.size(); i++) {
+            if (listOfEdges.get(i).getV2().getName() == name2) {
+                return listOfEdges.remove(i);
+            }
+        }
+        throw new IllegalArgumentException("No such edge");
+    }
 }
-
-
-/* Еще не знаю как всё это реализовывать
- * Поэтому напишу тут свои догадки по опитимизации и сокращению необходимой памяти
- */
-
-    /*Матрицы смежности - это матрица n*n, где n - кол-во вершин
-
-     В строке j колоны i стоит цифра один, если из вершины j в i есть путь длинной 1
-     Иначе в матрице стоит нуль.
-     Идея для упрощения: хранить матрицу в row-major представление, то есть двумерный массив как бы
-     сужается в одномерный  матрица | a b c | будет храниться как массив [a,b,c,d,e,f,j,k,l]
-                                    | d e f |
-                                    | j k l |
-     A[i*n+j] это элемеент в i-th строке и j-th столба
-     */
-
-    /*Матрица инцидентности - строки вершины графа, а столбцы - ребра графа
-
-      n - кол-во столбцов
-      A[i*n+j] = 1, если ребро выходит из вершины
-               = -1, если входит в вершину
-               = 0, если ребра нет или тут петля
-     Храним также как матрицу смежности
-     */
--- Task_1_2_3/src/test/java/ru/nsu/mbogdanov2/GraphTest.java
+++ Task_1_2_3/src/test/java/ru/nsu/mbogdanov2/GraphTest.java
@@ -1,4 +1,36 @@
 package ru.nsu.mbogdanov2;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Special class for testing graph methods.
+ */
 public class GraphTest {
+    @Test
+    public void matrixOfIncidentTest() throws FileNotFoundException {
+        File file = new File("src/test/resources/matrixOfIncidentTest.txt");
+        Scanner sc = new Scanner(file);
+        String vertexesLine = sc.nextLine();
+        int[] vertexArrayForInt = Arrays.stream(vertexesLine.split("\\s"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        Integer[] vertexArray = Arrays.stream( vertexArrayForInt ).boxed().toArray( Integer[]::new );
+        int numberOfEdges = sc.nextInt();
+        int numberOfVertexes = vertexArray.length;
+        int[] matrixOfIncident = new int[numberOfVertexes * numberOfEdges];
+        for( int i = 0; i < numberOfVertexes; i++) {
+            for( int j = 0; j < numberOfEdges; j++) {
+                matrixOfIncident[i * numberOfEdges + j] = sc.nextInt();
+            }
+        }
+        Graph<Integer> graph = new Graph<>(vertexArray,matrixOfIncident,numberOfEdges);
+        System.out.println("Hello world");
+        Assertions.assertEquals(numberOfEdges, vertexArray[0]);
+    }
 }
