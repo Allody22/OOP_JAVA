13b53dc14e367a564169ccd5ba3963c59b791d32
Миша <m.bogdanov2@g.nsu.ru>
2022-11-02 22:52:16 +0700

    + review dog fixes (nu nevozmogno zrazy sdelat pravilno, sorry)

--- Task_1_2_3/src/test/java/ru/nsu/mbogdanov2/EdgeTest.java
+++ Task_1_2_3/src/test/java/ru/nsu/mbogdanov2/EdgeTest.java
@@ -22,15 +22,15 @@ public class EdgeTest {
 
     @Test
     public void deleteEdgeTest() {
-        Graph<String> expectedGraph = new Graph<>();
         var startVertex = new Vertex<>("one");
         var secondVertex = new Vertex<>("two");
 
-        var firstEdge = new Edge<>(startVertex,secondVertex,3);
+        var firstEdge = new Edge<>(startVertex, secondVertex, 3);
         Assertions.assertEquals(firstEdge.getWeight(), 3);
         Assertions.assertEquals(firstEdge.getV1(), startVertex);
         Assertions.assertEquals(firstEdge.getV2(), secondVertex);
 
+        Graph<String> expectedGraph = new Graph<>();
         expectedGraph.addVertex("one");
         expectedGraph.addVertex("two");
         expectedGraph.addEdge("one", "two", 33);
@@ -89,9 +89,9 @@ public class EdgeTest {
         var actualVertexTwo = new Vertex<>("two");
         var actualVertexOne = new Vertex<>("one");
 
-        var expectedEdge = new Edge<>(actualVertexOne,actualVertexTwo,22);
-        Assertions.assertEquals(expectedEdge.getWeight(),22);
-        Assertions.assertNotEquals(expectedEdge.getWeight(),32);
+        var expectedEdge = new Edge<>(actualVertexOne, actualVertexTwo, 22);
+        Assertions.assertEquals(expectedEdge.getWeight(), 22);
+        Assertions.assertNotEquals(expectedEdge.getWeight(), 32);
 
         Assertions.assertNotEquals(expectedGraph.getEdge("one", "two", 33).getWeight(), 32);
         Assertions.assertEquals(expectedGraph.getEdge("one", "two", 33).getWeight(), 33);
@@ -109,8 +109,10 @@ public class EdgeTest {
         expectedGraph.addEdge("one", "two", 66);
 
         var expectedSecondVertex = new Vertex<>("two");
-        Assertions.assertEquals(expectedSecondVertex.getName(), expectedGraph.getEdge("one", "two", 33).getV2().getName());
-        Assertions.assertEquals(expectedSecondVertex.getName(), expectedGraph.getEdge("one", "two", 66).getV2().getName());
+        Assertions.assertEquals(expectedSecondVertex.getName(),
+                expectedGraph.getEdge("one", "two", 33).getV2().getName());
+        Assertions.assertEquals(expectedSecondVertex.getName(),
+                expectedGraph.getEdge("one", "two", 66).getV2().getName());
     }
 
     @Test
@@ -123,7 +125,9 @@ public class EdgeTest {
         expectedGraph.addEdge("one", "two", 66);
 
         var expectedSecondVertex = new Vertex<>("one");
-        Assertions.assertEquals(expectedSecondVertex.getName(), expectedGraph.getEdge("one", "two", 33).getV1().getName());
-        Assertions.assertEquals(expectedSecondVertex.getName(), expectedGraph.getEdge("one", "two", 66).getV1().getName());
+        Assertions.assertEquals(expectedSecondVertex.getName(),
+                expectedGraph.getEdge("one", "two", 33).getV1().getName());
+        Assertions.assertEquals(expectedSecondVertex.getName(),
+                expectedGraph.getEdge("one", "two", 66).getV1().getName());
     }
 }
