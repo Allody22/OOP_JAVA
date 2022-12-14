96fbc62139115cb8013a9025035cafc4065338cb
Allody22 <Allody22@users.noreply.github.com>
2022-12-14 12:49:11 +0000

    Deploying to gh-pages from @ Allody22/OOP_JAVA@8e2ca3ef011cac919fb45586ba611ac5c1dc6623 🚀

--- Task_1_5_1/package-search-index.js
+++ Task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.mbogdanov2"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.mbogdanov2"}];updateSearchResults();
