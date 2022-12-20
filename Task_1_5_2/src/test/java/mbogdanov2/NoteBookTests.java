61fd37e31ab672bf8c15bd4bde2aae2cade718d1
Миша <m.bogdanov2@g.nsu.ru>
2022-12-20 21:08:43 +0700

    Let's try to test this code like this

--- Task_1_5_2/src/test/java/mbogdanov2/NoteBookTests.java
+++ Task_1_5_2/src/test/java/mbogdanov2/NoteBookTests.java
@@ -1,17 +1,43 @@
 package mbogdanov2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.text.ParseException;
+import java.util.Objects;
 
 public class NoteBookTests {
     @Test
-    void addNoteTest() throws ParseException {
-        NoteBook.main(new String[]{"-add", "Моя заметка", "Очень важная заметка"});
+    void addNoteTest() throws ParseException, NoSuchFieldException, IllegalAccessException {
+        File notes = new File("notes.json");
+        if (notes.exists()) {
+            notes.delete();
+        }
+        NoteBook.main(new String[]{"-add", "Моя заметка 1", "Очень важная заметка 1"});
+        NoteBook.main(new String[]{"-add", "Моя заметка 2", "Очень важная заметка 2"});
+        var notesField = NoteBook.class.getDeclaredField("notesArray");
+        notesField.setAccessible(true);
+        var arrayOfNotes = (ArrayOfNotes) notesField.get(notesField);
+        Assertions.assertTrue(arrayOfNotes.checkExistence("Моя заметка 1"));
+        Assertions.assertFalse(arrayOfNotes.checkExistence("Моя заметка 33"));
+        notes.delete();
     }
-    @Test
-    void ShowNotesTest() throws ParseException {
-        NoteBook.main(new String[]{"-show", "Dec 1, 2022, 1:50:04 PM", "Dec 29, 2022, 1:50:04 PM"});
 
+    @Test
+    void RemoteNoteTest() throws ParseException, NoSuchFieldException, IllegalAccessException {
+        File notes = new File("notes.json");
+        if (notes.exists()) {
+            Assertions.assertTrue(notes.delete());
+        }
+        NoteBook.main(new String[]{"-add", "Моя заметка 1", "Очень важная заметка 1"});
+        NoteBook.main(new String[]{"-add", "Моя заметка 2", "Очень важная заметка 2"});
+        var notesField = NoteBook.class.getDeclaredField("notesArray");
+        notesField.setAccessible(true);
+        var arrayOfNotes = (ArrayOfNotes) notesField.get(notesField);
+        Assertions.assertTrue(arrayOfNotes.checkExistence("Моя заметка 1"));
+        NoteBook.main(new String[]{"-rm", "Моя заметка 1"});
+        Assertions.assertFalse(arrayOfNotes.checkExistence("Моя заметка 1"));
+        notes.delete();
     }
 }
