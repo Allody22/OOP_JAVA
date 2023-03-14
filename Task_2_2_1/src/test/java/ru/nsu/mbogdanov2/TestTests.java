111320da261d568eb0129c91d68f4dda415947f9
Миша <m.bogdanov2@g.nsu.ru>
2023-03-14 10:29:48 +0700

    Problem is fixed, now lets add some test

---
+++ Task_2_2_1/src/test/java/ru/nsu/mbogdanov2/TestTests.java
@@ -0,0 +1,11 @@
+package ru.nsu.mbogdanov2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTests {
+    @Test
+    public void NiceTest() {
+        Assertions.assertTrue(true);
+    }
+}
