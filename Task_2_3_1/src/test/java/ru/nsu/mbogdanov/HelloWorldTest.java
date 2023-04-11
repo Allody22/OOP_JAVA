7edeaabde262dfb927f877c10c5fd0ee5be1bdf4
Миша <m.bogdanov2@g.nsu.ru>
2023-04-11 19:44:49 +0700

    Добавил тест (кажется он нужен для сборки)

---
+++ Task_2_3_1/src/test/java/ru/nsu/mbogdanov/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.mbogdanov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testingTest(){
+        int i = 1;
+        Assertions.assertEquals(1,i);
+    }
+}
